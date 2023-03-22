47a983e5f1e2ab28f9e6ca1d907c000e2fbbfc1f
usilgtfoms <usilgtfoms@irkoms.ru>
2023-03-22 14:16:55 +0800

    Added plugin to ViteJs for copy bundle file in wwwroot folder

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    copy({
+      targets: [
+        { src: 'dist/assets', dest: '../CSVReader/wwwroot/' }
+      ]
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
